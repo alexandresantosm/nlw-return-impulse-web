c9291af821991242675d57fb6560c7d8509900eb
Alexandre Menezes <amenezes031@gmail.com>
2022-05-03 10:51:44 -0300

    feat: add Header, Main and Widget components in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import { Widget } from './components/Widget'
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+import { Widget } from './components/Widget';
 
 export const App: React.FC = () => {
-  return <Widget />
-}
+  return (
+    <div className="w-full h-full pb-8">
+      <Header />
+      <Main />
+      <Widget />
+    </div>
+  );
+};
